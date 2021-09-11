0993de174336970441de7e3bb046269f31612e02
xiaogit00 <leibingguo@gmail.com>
2021-09-11 15:20:06 +0800

    refactored model and router

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,21 +1,17 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogsRouter')
+const mongoose = require('mongoose')
 
 app.use(express.json())
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 
 mongoose.connect(mongoUrl)
   .then(() => console.log("connected to MongoDB"))
+
+
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -10,25 +10,6 @@ const cors = require('cors')
 
 app.use(cors())
 
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
 const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
