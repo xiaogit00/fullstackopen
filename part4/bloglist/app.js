84ee7aa0fa76c5a5947740febe2b5f99ba1e2811
xiaogit00 <leibingguo@gmail.com>
2021-09-12 12:34:34 +0800

    finished ex 4.2

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,15 +3,20 @@ const app = express()
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogsRouter')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 app.use(express.json())
 
+logger.info('connecting to', config.MONGODB_URI)
 const mongoUrl = config.MONGODB_URI
 
 mongoose.connect(mongoUrl)
-  .then(() => console.log("connected to MongoDB"))
+  .then(() => logger.info("connected to MongoDB"))
+  .catch((error) => logger.error('error in connecting to MongoDB', error.message))
 
-
+app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 
+app.use(middleware.unknownEndpoint)
 module.exports = app
